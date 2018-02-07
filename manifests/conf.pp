8e542f41aff47bf055c9d2cac6512bb2748ac7b5
Simon Deziel <simon@sdeziel.info>
2018-02-07 17:55:01 -0500

    Support removing conf snippets

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -1,11 +1,23 @@
 # Define: unbound::conf
 #
 define unbound::conf (
-  Hash $options,
+  Enum['present','absent'] $ensure  = 'present',
+  Optional[Hash] $options           = undef,
   Stdlib::Absolutepath $target_file = "${unbound::cfg_dir}/${name}",
 ) {
+  if $ensure == 'present' and ! $options {
+    fail("${module_name} requires an options hash to be defined when the conf is present")
+  }
+
+  if $ensure == 'present' {
+    $content = epp('unbound/conf.epp', { 'options' => $options })
+  } else {
+    $content = undef
+  }
+
   file { $target_file:
-    content => epp('unbound/conf.epp', { 'options' => $options }),
+    ensure  => $ensure,
+    content => $content,
     owner   => 0,
     group   => 0,
     mode    => '0644',
